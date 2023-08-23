c89c3ad7d5723731722e3bd74cd223f2efc6289f
siddhesh zope <siddheshzope1023@gmail.com>
2023-08-23 18:23:02 +0530

    Tuple data type

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -1,4 +1,6 @@
 fn main() {
+    //ARRAYS
+
     let primes:[i32;5] = [2,3,5,7,11];
     let double:[f64;4] = [2.8,4.8,4.5,7.89];
 
@@ -22,4 +24,41 @@ fn main() {
     for number in numbers.iter() {
         println!("{}",number);
     }
+
+
+    //VECTORS
+    //vectors are arrays of variable size
+    //Two ways to create:
+    // 1 Create object of Vec like Vec::new ()
+    // 2 Create macro vec!
+
+    let vecPrimes : Vec<i32> = Vec::new();
+    let mut vecPrimes = vec![2,3,5];
+
+    println!("{:?}",vecPrimes);
+
+    vecPrimes.push(7);
+    println!("{:?}",vecPrimes);
+    vecPrimes.remove(2);
+    println!("{:?}",vecPrimes);
+
+    let mut vecNumbers = vec![2;10];
+    println!("{:?}",vecNumbers);
+
+    const vecDEFAULT: bool = true;
+
+    let values = vec![vecDEFAULT;8];
+    println!("{:?}",values);
+
+    //update element
+
+    vecNumbers[5] = 8;
+    println!("{:?}",vecNumbers);
+
+    for number in vecNumbers.iter(){
+        println!("{:?}",number*number);
+    }
+
+
+
 }
